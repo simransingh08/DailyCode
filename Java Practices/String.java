a3fcff9efe30c9e8864953f037beb09c7b17c254
Simran <107848987+simransingh08@users.noreply.github.com>
2023-03-23 01:46:18 +0530

    New String program has been updated

---
+++ Java Practices/String.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class String1
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("May I know your name Please!");
+        name = sc.next();
+        System.out.println("Ooh nice Name" + "  " + "Welcome" +" "  + name);
+    }
+}
